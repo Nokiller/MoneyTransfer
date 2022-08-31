9b8b9a4cafa6db75c31e0e69f81f135ea5ed7279
Nikita Chubukov <NikitaCH@bk.ru>
2022-08-31 18:45:32 +0300

    The value of minimum fee is moved to a separate variable

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,6 +1,7 @@
 fun main() {
     val amount = 100_000 // Сумма перевода в копейках
     val tax = 0.0075 //Ставка по комиссии за перевод 0,75%
-    val fee = if (amount * tax >= 3500) amount * tax else 3500
+    val minimumFee = 3500 //Минимальый размер комиссии
+    val fee = if (amount * tax >= minimumFee) amount * tax else minimumFee
     println("Комиссия за перевод $amount коп. составит: $fee коп.")
 }
